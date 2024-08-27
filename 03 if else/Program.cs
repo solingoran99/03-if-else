b5a4e0dd210f7fcadd6e45e4c0fb9642de743c30
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2024-08-27 18:13:10 +0200

    Add project files.

---
+++ 03 if else/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03_if_else
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//1. Even or Odd:
+
+			double userInput;
+			Console.WriteLine("Enter a number");
+			userInput = double.Parse(Console.ReadLine());
+
+			if (userInput %2 == 0)
+			{
+                Console.WriteLine("Number is even");
+            }
+			else
+			{
+				Console.WriteLine("Number is odd");
+			}
+			Console.ReadKey();
+		}
+	}
+}
