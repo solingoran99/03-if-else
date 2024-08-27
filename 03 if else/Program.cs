284d9ff8e83c582b61dcca32b3cf19a7700ffbcc
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2024-08-27 19:17:05 +0200

    task 2

--- 03 if else/Program.cs
+++ 03 if else/Program.cs
@@ -7,7 +7,7 @@
 			//1. Even or Odd:
 
 			double userInput;
-			Console.WriteLine("Enter a number");
+			Console.WriteLine("Enter a number:");
 			userInput = double.Parse(Console.ReadLine());
 
 			if (userInput %2 == 0)
@@ -18,7 +18,27 @@
 			{
 				Console.WriteLine("Number is odd");
 			}
+			
+            //2. Age Bracket
+
+            Console.WriteLine("Enter your age.");
+			int userAge = int.Parse(Console.ReadLine());
+
+			if (userAge < 13)
+			{
+                Console.WriteLine("You are a child.");
+            }
+			else if (userAge >= 13 &&  userAge <= 19)
+			{
+                Console.WriteLine("You're a teenager");
+            }
+			else
+			{
+                Console.WriteLine("You are an adult.");
+            }
 			Console.ReadKey();
+
+
 		}
 	}
 }
